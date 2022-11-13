19bd399c4ce00c18e68385143a69f675ac6fd55b
mrwilliambarenga <williambarenga@gmail.com>
2022-11-13 17:43:29 +0000

    Create ThingForm

--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from things.models import Thing
 
 # Create your tests here.
+class ThingsModelTestCase(TestCase):
+    def setUp(self):
+        self.thing = Thing.objects.create(
+            name='John',
+            description='The quick brown fox jumps over the lazy frog',
+            quantity=6
+        )
